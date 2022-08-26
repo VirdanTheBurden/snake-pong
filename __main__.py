0a13de3cf9f4dff6350f4de5d773cedd74461913
Luna <ojackson287@gmail.com>
2022-08-26 13:11:04 -0400

    feat: main window

---
+++ __main__.py
@@ -0,0 +1,27 @@
+# TODO: field initialization, apple seeding, paddle spawning, etc.
+
+import arcade
+
+# Constants, do not touch
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 1920
+TITLE = "Snake Pong"
+
+class SnakePong(arcade.Window):
+    """Main application class."""
+
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
+        arcade.set_background_color(arcade.color.GRAY)
+    
+    def setup(self):
+        """Draw paddle and snake."""
+        pass
+
+    def on_draw(self):
+        self.clear()
+
+def main():
+    window = SnakePong()
+    window.setup()
+    arcade.run()
